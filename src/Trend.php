839154e2b21b2a3485aee6b4d4e0c25106d0b88e
Michael N <penguinus@gmail.com>
2024-08-21 23:15:23 +0300

    Fix

--- src/Trend.php
+++ src/Trend.php
@@ -99,8 +99,8 @@ class Trend
                 {$this->getSqlDate()} AT TIME ZONE '" . config('app.admin_timezone') . "' as {$this->dateAlias},
                 {$aggregate}({$column}) as aggregate
             ")
-//            ->whereBetween($this->dateColumn, [$this->start, $this->end])
-            ->whereRaw($this->dateColumn . " AT TIME ZONE '" . config('app.admin_timezone') . "' BETWEEN '" . $this->start . "' AND '" . $this->end . "'")
+            ->whereBetween($this->dateColumn, [$this->start, $this->end])
+//            ->whereRaw($this->dateColumn . " AT TIME ZONE '" . config('app.admin_timezone') . "' BETWEEN '" . $this->start . "' AND '" . $this->end . "'")
             ->groupBy($this->dateAlias)
             ->orderBy($this->dateAlias)
             ->get();
